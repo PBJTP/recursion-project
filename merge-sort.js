42b36c8cc304e54c67ef80a2946e337dd483985d
PBJTP <jplypick1990@gmail.com>
2023-02-23 00:05:23 -0800

    Finish recursion Fibonacci problem. Pseudocode for merge-sort

---
+++ merge-sort.js
@@ -0,0 +1,14 @@
+//Merge sort algorithm using recursion
+
+
+//accept array as argument
+
+//Find middle of array
+
+//SLICE array in a half, left and right
+
+//continue SLICE array halves until each is only 1
+
+//COMPARISON each piece as they are CONCAT back together
+
+//continue until full array again
