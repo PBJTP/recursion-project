fec5de0fdbc8e20464af81b7130089407d33d1ce
PBJTP <jplypick1990@gmail.com>
2023-02-23 00:10:34 -0800

    Find middle of array, rounding down

--- merge-sort.js
+++ merge-sort.js
@@ -12,3 +12,12 @@
 //COMPARISON each piece as they are CONCAT back together
 
 //continue until full array again
+
+function mergeSort(array) {
+    const middle = Math.floor(array.length / 2);
+    return middle;
+};
+
+
+
+console.log(mergeSort([5,3,123,65,789,223,214,5,6,1,67,8,5]));
