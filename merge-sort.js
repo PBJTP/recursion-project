ab3773bb6811e3970154cc17714e9f2b135c05b8
PBJTP <jplypick1990@gmail.com>
2023-02-23 22:39:52 -0800

    Finish solution

--- merge-sort.js
+++ merge-sort.js
@@ -9,16 +9,34 @@
 
 //continue SLICE array halves until each is only 1
 
-//COMPARISON each piece as they are CONCAT back together
+//COMPARISON each piece as they are CONCAT back together. Ended up using a spread here instead of concat
 
-//continue until full array again
+//continue until fullly sorted array
 
 function mergeSort(array) {
+    //base case
+    if (array.length < 2) return array;
+
     const middle = Math.floor(array.length / 2);
     const left = array.slice(0, middle);
     const right = array.slice(middle);
-    return [left, right];
+    
+    //recursion
+    return merge(mergeSort(left), mergeSort(right));
 };
+
+function merge(left, right) {
+    let sortedArray = [];
+    while (left.length && right.length) {
+        if (left[0] <= right[0]) {
+            sortedArray.push(left.shift());
+        } else {
+            sortedArray.push(right.shift());
+        }
+    };
+
+    return [...sortedArray, ...left, ...right];
+}
 
 
 
